4dbab5263f27fa764d617d4d23fec265515a5bb9
Your Name <you@example.com>
2026-04-10 20:58:38 +0300

    text

--- main/views.py
+++ main/views.py
@@ -5,4 +5,5 @@ from .models import Certificate
 def certificate_detail(request, certificate_number):
     """Sertifikatni raqami bo'yicha ko'rsatish"""
     certificate = get_object_or_404(Certificate, certificate_number=certificate_number)
-    return render(request, 'certificate_detail.html', {'certificate': certificate})
+    return render(request, 'text.html', {'certificate': certificate})
+
